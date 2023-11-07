57132723f90cbffc68a109ef95dc5e5f66005892
spitalskya <spitalskya@gmail.com>
2023-11-07 18:52:06 +0100

    Created WallLine class and test files

---
+++ azul/wall_line.py
@@ -0,0 +1,2 @@
+class WallLine:
+    pass
---
+++ test/test_wall_line.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import unittest
+from typing import List
+from azul.wall_line import WallLine
+
+
+class TestWallLine(unittest.TestCase):
+    pass
