4f0025e49ff7fa23b7697e4a8f8140462bca9110
spitalskya <spitalskya@gmail.com>
2023-11-07 21:53:53 +0100

    Basic structure of WallLine class

--- azul/wall_line.py
+++ azul/wall_line.py
@@ -1,2 +1,26 @@
+from __future__ import annotations
+from typing import List, Optional
+from itertools import chain, repeat, islice
+from azul.interfaces import UsedTilesGiveInterface
+from azul.simple_types import Tile, compress_tile_list, Points
+
 class WallLine:
-    pass
+    _tileTypes: List[Tile]
+    _tiles: List[Tile]
+    _lineUp: Optional[WallLine]
+    _lineDown: Optional[WallLine]
+    
+    def __init__(self, tileTypes: List[Tile]) -> None:
+        self._tileTypes = tileTypes
+    
+    def canPutTile(self, tile: Tile) -> bool:
+        pass
+    
+    def getTiles(self) -> Optional[List[Tile]]:
+        pass
+    
+    def putTile(self, tile: Tile) -> Points:
+        pass
+    
+    def state(self) -> str:
+        return compress_tile_list(self._tiles)
