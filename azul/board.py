52d7e2470b34d711e402297c8a65849ae20c7738
erikbozik <erik23.bozik@gmail.com>
2023-11-12 00:05:47 +0100

    init and endGame written.

--- azul/board.py
+++ azul/board.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import List
-from azul.simple_types import Tile, FinishRoundResult, Points
+from azul.simple_types import Tile, FinishRoundResult, Points, RED, BLUE, YELLOW, GREEN, BLACK, NORMAL, GAME_FINISHED,\
+    compress_tile_list
 from azul.interfaces import GameFinishedInterface, FinalPointsCalculationInterface, UsedTilesGiveInterface
 from azul.floor import Floor
 
@@ -16,7 +17,38 @@ class Board:
 
     def __init__(self, game_finished: GameFinishedInterface, final_points: FinalPointsCalculationInterface,
                  used_tiles: UsedTilesGiveInterface) -> None:
-        ...
+        self.game_finished = game_finished
+        self.final_points = final_points
+
+        points_pattern: List[Points] = [Points(1), Points(1),
+                                        Points(2), Points(2), Points(2),
+                                        Points(3), Points(3)]
+        self.floor = Floor(points_pattern, used_tiles)
+
+        # NOT IMPLEMENTED PART YET
+        self.pattern_lines = [NotImplemented for capacity in range(1, 6)]
+
+        wall_lines_pattern: List[Tile] = [
+            [BLUE, YELLOW, RED, BLACK, GREEN],
+            [GREEN, BLUE, YELLOW, RED, BLACK],
+            [BLACK, GREEN, BLUE, YELLOW, RED],
+            [RED, BLACK, GREEN, BLUE, YELLOW],
+            [YELLOW, RED, BLACK, GREEN, BLUE]
+        ]
+        self.wall_lines = [NotImplemented for w_pattern in wall_lines_pattern]
+
+        for index, wall_line in enumerate(self.wall_lines):
+            try:
+                wall_line.put_line_up(self.wall_lines[index - 1])
+            except IndexError:
+                pass
+
+            try:
+                wall_line.put_line_down(self.wall_lines[index + 1])
+            except IndexError:
+                pass
+
+        self.points = Points(0)
 
     def put(self, destination: int, tiles: List[Tile]) -> None:
         return NotImplemented
@@ -25,7 +57,13 @@ class Board:
         return NotImplemented
 
     def endGame(self) -> None:
-        return NotImplemented
+        wall_lines = [w_line.get_tiles() for w_line in self.wall_lines]
+        finish: FinishRoundResult = self.game_finished.gameFinished(wall_lines)
+        if finish == NORMAL:
+            self.end_game = False
+
+        if finish == GAME_FINISHED:
+            self.end_game = True
 
     def state(self) -> str:
-        return NotImplemented
+        pass
