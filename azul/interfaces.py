47dca10a10ebde64856ab6f14efcb226e17bdd0f
Rafael Rohal <92051602+huncut47@users.noreply.github.com>
2023-11-12 00:32:52 +0100

    Implemented ABC and abstract method

--- azul/interfaces.py
+++ azul/interfaces.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 from typing import List, Optional
+from abc import ABC, abstractmethod
 from azul.simple_types import Tile, Points
 
 
-class UsedTilesGiveInterface:
+class UsedTilesGiveInterface(ABC):
     def give(self, tiles: List[Tile]) -> None:
         pass
 
 
 class FinalPointsCalculationInterface:
+    
+    @abstractmethod
     def get_points(self, wall: List[List[Optional[Tile]]]) -> Points:
         pass
