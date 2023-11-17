80a177c67ab8bc5da12165750cbff4d31ef5f15a
antal36 <antal.tom9@gmail.com>
2023-11-17 16:47:22 +0100

    more mypy changes

--- azul/factory.py
+++ azul/factory.py
@@ -1,23 +1,22 @@
 from __future__ import annotations
 from typing import List
-from abc import ABC, abstractmethod
-from azul.simple_types import Tile
+from azul.simple_types import Tile, RED
 from azul.interfaces import FactoryBagInterface, TileSource
 from azul.table_center import TableCenter
 
 
 class Factory(TileSource):
-    def __init__(self, bag: FactoryBagInterface, table_center: TableCenter) -> None:
+    def __init__(self, bag: FactoryBagInterface, table_center: TileSource) -> None:
         pass
 
     def take(self, idx: Tile) -> List[Tile]:
-        pass
+        return [RED]
 
     def is_empty(self) -> bool:
-        pass
+        return True
 
     def state(self) -> str:
-        pass
+        return ""
 
     def start_new_round(self) -> None:
         pass
