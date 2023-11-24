109f57bd76760bdc82fbeb8b73cc84ef29f0d44c
teoPazera <pazerateo@gmail.com>
2023-11-24 14:58:12 +0100

    extended test for bag

--- test/test_bag.py
+++ test/test_bag.py
@@ -1,29 +1,46 @@
 from __future__ import annotations
 import unittest
 from typing import List
-from azul.simple_types import Tile, compress_tile_list, RED, GREEN, BLACK, BLUE, YELLOW
+from azul.simple_types import Tile, RED, GREEN, BLACK, BLUE, YELLOW, compress_tile_list
 from azul.interfaces import TestUsedTilesTakeAllInterface, UsedTilesTakeAllInterface, TestRngInterface, RngInterface
 from azul.bag import Bag
 
 class TestBag(unittest.TestCase):
-    _tiles: List[Tile]
-    def set_up(self) -> None:
+    _tiles:List[Tile]
+    def setUp(self) -> None:
         used_tiles: UsedTilesTakeAllInterface =  TestUsedTilesTakeAllInterface()
         rng: RngInterface = TestRngInterface()
         self.bag: Bag = Bag(used_tiles, rng)
     
     def test_bag1(self) -> None:
-        self.assertEqual(len(self.bag.state()), 100)
+        correct_order: List[Tile] = []
+        for _ in range(20):
+            correct_order.extend([RED, GREEN, BLACK, BLUE, YELLOW])
+        self.assertEqual(compress_tile_list(correct_order), self.bag.state())
+        self.assertEqual('RGLBY'*20, self.bag.state())
         _tiles = self.bag.take(4)
         self.assertEqual(_tiles, [RED, GREEN, BLACK, BLUE])
         _tiles = self.bag.take(4)
         self.assertEqual(_tiles, [YELLOW, RED, GREEN, BLACK])
-        for i in range(23):
-            self.bag.take(4)
-        self.assertTrue(len(self.state()), 0)
+        _tiles.extend(self.bag.take(4))
+        self.assertEqual(_tiles, [YELLOW, RED, GREEN, BLACK, BLUE, YELLOW, RED, GREEN])
+
+    def test_bag2(self) -> None:
+        _tiles = []
+        for i in range(25):
+            _tiles.extend(self.bag.take(4))
+        self.assertEqual(len(_tiles), 100)
+        self.assertEqual(self.bag.state(), "")
         #emptied bag will draw more tiles from usedtiles
         _tiles = self.bag.take(4)
         self.assertEqual(_tiles, [RED]*4)
-        self.assertEqual(self.bag.state(),[RED] * 6 + [BLACK] * 12+ [GREEN] * 11)
+        self.assertEqual(self.bag.state(),compress_tile_list([RED] * 6 + [BLACK] * 12 + [GREEN] * 11))
 
+    def test_bag3(self) -> None:
+        _tiles = []
+        for i in range(5):
+            _tiles.extend(self.bag.take(4))
+        self.assertEqual(len(_tiles),20)
+        self.assertEqual('RGLBY'*4, compress_tile_list(_tiles))
+        
 
